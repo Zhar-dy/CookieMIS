0f48a8b1a573fd02d727d1888def6f56fa3a6fe1
RexyRanger <rexyranger03@gmail.com>
2024-05-30 20:41:31 +0800

    Fixed the cookie logo not coming out

--- reusableCodes/header1in.php
+++ reusableCodes/header1in.php
@@ -1,7 +1,7 @@
 <!-- header section start -->
 <div class="header_section">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="logo"><a href="index.html"><img src="images/logo.png"></a></div>
+            <div class="logo"><a href="index.html"><img src="../images/logo.png"></a></div>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
