bdc3a70545bff72c06cf54a0cfba50047baea9fa
RexyRanger <rexyranger03@gmail.com>
2024-06-04 11:29:48 +0800

    Fixed Registeration System

--- register/process.php
+++ register/process.php
@@ -1,6 +1,6 @@
 <?php
 // Include database connection settings
-include('../include/dbconn.php');
+include('../reusableCodes/connectdb.php');
 
 if(isset($_POST['signup'])){
 	/* capture values from HTML form */
@@ -11,25 +11,41 @@ if(isset($_POST['signup'])){
 	$user_name = $_POST['username'];
 	$password = $_POST['password'];
 	$gender = $_POST['gender'];
-	$level = "user";
-
-	$sql0 = "SELECT username FROM user WHERE username = '$user_name'";
+	$level_id = 2;
+	// check if username already exists
+	$sql0 = "SELECT username FROM users WHERE username = '$user_name'";
 	$query0 = mysqli_query($conn, $sql0) or die ("Error: " . mysqli_error($conn));
 	$row0 = mysqli_num_rows($query0);
 	
 	if($row0 != 0){
-	header('Location: ../register/regRecExist.php');
-	//echo "Record is existed";
-	}
-	else{
-	/* execute SQL INSERT command */
-	$sql2 = "INSERT INTO user (username, password,name,gender,address,phonenum,email,level_id)
-	VALUES ('" . $user_name . "', '" . $password . "', '" . $name . "', '" . $gender . "', '" . $address . "', '" . $phoneno . "', '" . $email . "', '" . $level_id . "')";
-	
-	mysqli_query($conn, $sql2) or die ("Error: " . mysqli_error($conn));
-	/* rediredt to respective page */
-	header('Location: ../login/login.php');
-	//echo "Data has been saved";
+		header('Location: ../register/regRecExist.php');
+		//echo "Record is existed";
+	}else{
+		// Fetch and save file into a folder
+		// Handle file upload
+		$path = '..\\userPictures\\';
+		$tmplocation = $_FILES["file"]["tmp_name"];
+
+		$newFileName = $name; 
+		$newFileExtension = ".jpg"; 
+		$imageName=$newFileName.$newFileExtension;
+		// Save new picture first
+		if (!empty($_FILES["file"]["name"])) {
+			if (!move_uploaded_file($tmplocation, $path . $newFileName . $newFileExtension)) {
+				die("Error uploading file.");
+			}
+		}
+
+		/* execute SQL INSERT command */
+		$sql2 = "INSERT INTO users (username, name,password,gender,address,email,phone_Num,picture,level_id)
+		VALUES ('" . $user_name . "', '" . $name . "', '" . $password . "', '" . $gender . "', '" . $address . "', '" . $email . "', '" . $phoneno . "', '" . $imageName . "', '" . $level_id . "')";
+		
+		mysqli_query($conn, $sql2) or die ("Error: " . mysqli_error($conn));
+		/* rediredt to respective page */
+		// Close Connection
+		mysqli_close($conn);
+		header('Location: ../login/login.php');
+		//echo "Data has been saved";
 	}
 }// close if isset()
 	/* close db connection */
--- register/regRecExist.php
+++ register/regRecExist.php
@@ -18,50 +18,63 @@
           <div class="image_2"><img src="../images/testcook.png" alt="Register Image"></div>
         </div>
         <div class="col-md-6">
+          
           <h1 class="register_text">REGISTER</h1>
-          <form action="db_add_user.php" method="post" enctype="multipart/form-data">
+          <h5>(Record already exists!)</h5>
+          <form action="db_add_user.php" method="POST" enctype="multipart/form-data">
+            
             <div class="form-group">
               <label for="username">Username:</label>
               <input type="text" id="username" name="username" class="form-control" placeholder="Enter your username" required>
             </div>
+
             <div class="form-group">
               <label for="name">Name:</label>
               <input type="text" id="name" name="name" class="form-control" placeholder="Enter your name" required>
             </div>
+
             <div class="form-group">
               <label for="password">Password:</label>
               <input type="password" id="password" name="password" class="form-control" placeholder="Enter your password" required>
             </div>
+
             <div class="form-group">
               <label for="gender">Gender: </label>
               <div class="form-check form-check-inline">
                 <input class="form-check-input" type="radio" name="gender" id="male" value="1" required>
                 <label class="form-check-label" for="male">Male</label>
               </div>
+              
               <div class="form-check form-check-inline">
                 <input class="form-check-input" type="radio" name="gender" id="female" value="2" required>
                 <label class="form-check-label" for="female">Female</label>
               </div>
             </div>
+
             <div class="form-group">
               <label for="address">Address:</label>
               <input type="text" id="address" name="address" class="form-control" placeholder="Enter your address" required>
             </div>
+
             <div class="form-group">
               <label for="email">Email:</label>
               <input type="email" id="email" name="email" class="form-control" placeholder="Enter your email" required>
             </div>
+
             <div class="form-group">
               <label for="phone">Phone Number:</label>
               <input type="tel" id="phone" name="phonenum" class="form-control" placeholder="Enter your phone number" required>
             </div>
+
             <div class="form-group">
               <label for="file">Profile Picture:</label>
               <input type="file" id="file" name="file" class="form-control">
             </div>
+
             <input type="hidden" name="level" value="2" />
             <button type="submit" class="btn btn-primary" name="signup">REGISTER</button>
           </form>
+
         </div>
       </div>
     </div>
