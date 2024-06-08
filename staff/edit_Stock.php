bca71afd7c75563cd9f8106e56a5d34f720c5777
adamallow <adamisland46@gmail.com>
2024-06-08 23:56:23 +0800

    page for edit product

---
+++ staff/edit_Stock.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>CookieMIS</title>
+  <!-- Header Meta File -->
+  <?php include '../reusableCodes/headerMeta1in.php' ?>
+</head>
+<body>
+  <!-- Header File -->
+  <?php include '../reusableCodes/headerStaff.php' ?>
+  
+  <!-- view section -->
+  <section class="product_section">
+  <?php include '../reusableCodes/getProductsArray.php'?>
+
+        <div class="container">
+        <div class="container product_container">
+        
+        </div>
+    </section>
+  
+
+
+  <!-- view end -->>
+
+    <!-- Footer File -->
+    <?php include '../reusableCodes/footer1in.php' ?>
+</body>
+</html>
