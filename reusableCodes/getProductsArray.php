279161b2753cf8ef318cd93dc1502dd9c9912b79
RexyRanger <rexyranger03@gmail.com>
2024-06-08 22:41:55 +0800

    new function for view product

--- reusableCodes/getProductsArray.php
+++ reusableCodes/getProductsArray.php
@@ -65,6 +65,39 @@
 			}
 		}
 	}
+	function updateProduct() {
+		// Include database connection settings
+		include('connectdb.php');
+
+		$sql= "SELECT * FROM product";
+		$query = mysqli_query($conn, $sql);
+		// Get total rows of the data
+		$totalRows = mysqli_num_rows($query);
+		$arrayOfArrays = array();
+		// Store the arrays into an array
+		while($row = mysqli_fetch_array($query)){
+			$arrayOfArrays[] = $row;
+		}
+		for($i=0; $i<$totalRows; $i++){
+			echo"<div class=\"box\">
+                <div class=\"img-box\">
+				<img src=\"images/cookies/".$arrayOfArrays[$i][7]."\">
+                </div>
+                <div class=\"detail-box\">
+                    <h6>".$arrayOfArrays[$i][1]." <span>".$arrayOfArrays[$i][3]."</span></h6>
+                    <p class=\"long_text\">".$arrayOfArrays[$i][2]."</p>
+                    <h5>RM".$arrayOfArrays[$i][6]."</h5>";
+			if ($arrayOfArrays[$i][4]==="Available" && $arrayOfArrays[$i][5] >= 1){
+				echo "<a href=\"#\">Update Stock</a>
+                	</div>
+           			</div>";
+			}else{
+				echo "<a href=\"#\" style=\"background-color:red\">Refill Stock</a>
+                	</div>
+           			</div>";
+			}
+		}
+	}
 
 
 /*
