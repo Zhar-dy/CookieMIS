941cf4aaf14ec0e77c15c875ef6dc2df11317746
RexyRanger <rexyranger03@gmail.com>
2024-06-21 19:10:38 +0800

    [Urgent Update] view user (not complete)
    will be fiixng it

--- staff/view_User.php
+++ staff/view_User.php
@@ -23,44 +23,54 @@ session_start();
     <div class="container">
     <h1 class="about_text" style="text-align:center">View User Info</h1>
 
+
+    <!-- What you want to do here is basically save the id of each registered user in an array -->
+     <!-- and then you view user base on the selected button. But the question is, -->
+      <!-- How do you find the user ID, how to save it an array and fetch it later, and how to select the right thing? -->
+       <!-- More importantly, is the viewUser and view_Detailed_User the same thing? if it is, then copy paste...? -->
       <!--                 table start here            -->
       <h1 class="label" style="color:lightcoral">Staff: </h1>
-      <div class="table">
-        <!--no need to chang tbh-->
-        <div class="trow header">
-          <div class="cell">
-            Username
+      <form action="test.php" method="GET">
+      <input type="Hidden" class="btn btn-secondary" name="type" value="admin">
+        <div class="table">
+          <!--no need to chang tbh-->
+          <div class="trow header">
+            <div class="cell">
+              Username
+            </div>
+            <div class="cell">
+              Name
+            </div>
+            <div class="cell">
+              gender
+            </div>
+            <div class="cell">
+              Address
+            </div>
+            <div class="cell">
+              Email
+            </div>
+            <div class="cell">
+              Phone Number
+            </div>
+            <div class="cell" align="center">
+              Action
+            </div>
           </div>
-          <div class="cell">
-            Name
-          </div>
-          <div class="cell">
-            gender
-          </div>
-          <div class="cell">
-            Address
-          </div>
-          <div class="cell">
-            Email
-          </div>
-          <div class="cell">
-            Phone Number
-          </div>
-          <div class="cell" align="center">
-            Action
-          </div>
-        </div>
 
-        <!--REPEAT THIS BELOW with php magic-->
-        
-          <?php printStaff() ?>
-      </div>
+          <!--REPEAT THIS BELOW with php magic-->
+          
+            <?php printStaff() ?>
+        </div>
+        </form>
 
 
 
 
       <!-- NEW TABLE -->
       <h1 class="label" style="color:blueviolet">User: </h1>
+      <form action="test.php" method="GET">
+      <input type="Hidden" class="btn btn-secondary" name="type" value="user">
       <div class="table">
         <!--no need to chang tbh-->
         <div class="trow blue header">
@@ -90,7 +100,7 @@ session_start();
         <!--REPEAT THIS BELOW FOR customer with php magic-->
         <?php printUsers() ?>
       </div>
-    
+      </form>
     </div>
     <!-- Footer File -->
     <?php include '../reusableCodes/footer1in.php' ?>
