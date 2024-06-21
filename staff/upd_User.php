8150505da7d6e3f7c7162c0aad5667f6aca9b4f6
Zhardy <zhardydev@gmail.com>
2024-06-21 19:18:57 +0800

    update

--- staff/upd_User.php
+++ staff/upd_User.php
@@ -21,76 +21,78 @@ session_start();
   <!--,,-->
   <div class="product_section layout_padding">
     <div class="container">
-    <h1 class="about_text" style="text-align:center">View User Info</h1>
+      <h1 class="about_text" style="text-align:center">View User Info</h1>
 
       <!--                 table start here            -->
       <h1 class="label" style="color:lightcoral">Staff: </h1>
-      <div class="table">
-        <!--no need to chang tbh-->
-        <div class="trow header">
-          <div class="cell">
-            Username
+      <div class="table-container">
+        <div class="table">
+          <!--no need to chang tbh-->
+          <div class="trow header">
+            <div class="cell">
+              Username
+            </div>
+            <div class="cell">
+              Name
+            </div>
+            <div class="cell">
+              gender
+            </div>
+            <div class="cell">
+              Address
+            </div>
+            <div class="cell">
+              Email
+            </div>
+            <div class="cell">
+              Phone Number
+            </div>
+            <div class="cell" align="center">
+              Action
+            </div>
           </div>
-          <div class="cell">
-            Name
-          </div>
-          <div class="cell">
-            gender
-          </div>
-          <div class="cell">
-            Address
-          </div>
-          <div class="cell">
-            Email
-          </div>
-          <div class="cell">
-            Phone Number
-          </div>
-          <div class="cell" align="center">
-            Action
-          </div>
-        </div>
 
-        <!--REPEAT THIS BELOW with php magic-->
-        
+          <!--REPEAT THIS BELOW with php magic-->
+
           <?php printStaff() ?>
+        </div>
       </div>
-
 
 
 
       <!-- NEW TABLE -->
       <h1 class="label" style="color:blueviolet">User: </h1>
-      <div class="table">
-        <!--no need to chang tbh-->
-        <div class="trow blue header">
-          <div class="cell">
-            Username
+      <div class="table-container">
+        <div class="table">
+          <!--no need to chang tbh-->
+          <div class="trow blue header">
+            <div class="cell">
+              Username
+            </div>
+            <div class="cell">
+              Name
+            </div>
+            <div class="cell">
+              gender
+            </div>
+            <div class="cell">
+              Address
+            </div>
+            <div class="cell">
+              Email
+            </div>
+            <div class="cell">
+              Phone Number
+            </div>
+            <div class="cell" align="center">
+              Action
+            </div>
           </div>
-          <div class="cell">
-            Name
-          </div>
-          <div class="cell">
-            gender
-          </div>
-          <div class="cell">
-            Address
-          </div>
-          <div class="cell">
-            Email
-          </div>
-          <div class="cell">
-            Phone Number
-          </div>
-          <div class="cell" align="center" >
-            Action
-          </div>
-        </div>
 
-        <!--REPEAT THIS BELOW FOR customer with php magic-->
-        <?php printUsers() ?>
+          <!--REPEAT THIS BELOW FOR customer with php magic-->
+          <?php printUsers() ?>
+        </div>
       </div>
-    
     </div>
     <!-- Footer File -->
     <?php include '../reusableCodes/footer1in.php' ?>
