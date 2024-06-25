ca2cb2f5e9bd45fa6a8a79de3dc4bc089ca4513b
Zhardy <zhardydev@gmail.com>
2024-06-25 22:19:33 +0800

    update

--- upduser.php
+++ upduser.php
@@ -1,17 +1,19 @@
 <?php
-  session_start();
-  
+session_start();
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Update User</title>
   <!-- Header Meta File -->
   <?php include 'reusableCodes/headerMeta.php' ?>
 </head>
+
 <body>
-	<!-- Header File -->
+  <!-- Header File -->
   <?php include 'reusableCodes/header.php';
   if (!isset($_SESSION['name'])) {
     header('Location: index.php');
@@ -28,29 +30,36 @@
           <form method="POST" action="./reusableCodes/updateUser.php">
             <div class="form-group">
               <label for="name">Name:</label>
-              <input type="text" class="form-control" id="name" name = "name" placeholder="<?php echo $_SESSION['name']?>">
+              <input type="text" class="form-control" id="name" name="name" placeholder="<?php echo $_SESSION['name'] ?>">
             </div>
 
 
             <div class="form-group">
               <label for="email">Email:</label>
-              <input type="email" class="form-control" id="email" name = "email" placeholder="<?php echo $_SESSION["email"]?>">
+              <input type="email" class="form-control" id="email" name="email" placeholder="<?php echo $_SESSION["email"] ?>">
             </div>
 
             <div class="form-group">
               <label for="password">Password:</label>
-              <input type="password" class="form-control" id="password" name = "password" placeholder="Enter your password">
+              <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password">
             </div>
 
             <div class="form-group">
               <label for="phone">Phone:</label>
-              <input type="tel" class="form-control" id="phone" name = "phone" placeholder="<?php echo $_SESSION["phone_Num"]?>">
+              <input type="tel" class="form-control" id="phone" name="phone" placeholder="<?php echo $_SESSION["phone_Num"] ?>">
             </div>
 
             <p>Note: You will need to log in again after updating your account</p>
             <button type="submit" class="btn btn-primary" name="update">Update Profile</button>
           </form>
-
+        </div>
+        <div class="col-md-6">
+          <h1 class="update_text">Update Picture</h1>
+          <div class="form-group">
+            <label for="picture">Picture:</label>
+            <input type="file" name="file" id="file" />
+            <img src="images/<?php echo $_SESSION['picture']; ?>" width="130" height="130">
+          </div>
         </div>
       </div>
     </div>
