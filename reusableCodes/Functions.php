f677f1486f0eddb0b2952b1ca4fc7a0160d61d5b
RexyRanger <rexyranger03@gmail.com>
2024-07-06 13:21:38 +0800

    Delete User Function patch

--- reusableCodes/Functions.php
+++ reusableCodes/Functions.php
@@ -463,6 +463,9 @@ function printStaff()
 		} elseif ($current_page == "upd_User") {
 			echo "<button type='submit' class='btn btn-primary' name='update' value='" . $arrayOfArrays[$i][0] . "'" . $arrayOfArrays[$i][0] . "'>
 				Update Profile
+			  </button><br>
+			  <button type='submit' class='btn btn-secondary' name='delete' value='" . $arrayOfArrays[$i][0] . "'" . $arrayOfArrays[$i][0] . "'>
+				Delete Profile
 			  </button>"; // goes "updateUser.php"
 		}
 
@@ -547,7 +550,11 @@ function printUsers()
 			echo "<button type=\"submit\" class=\"btn btn-secondary\" name=\"view\" value=\"" . $arrayOfArrays[$i][0] . "\">View Profile</button>"; // goes view_Detail_User.php
 		} elseif ($current_page == "upd_User") {
 
-			echo "<button type=\"submit\" class=\"btn btn-secondary\" name=\"update\" value='" . $arrayOfArrays[$i][0] . "'>Update Profile</button>"; // goes "updateUser.php"
+			echo "<button type=\"submit\" class=\"btn btn-primary\" name=\"update\" value='" . $arrayOfArrays[$i][0] . "'>Update Profile</button>
+			<button type='submit' class='btn btn-secondary' name='delete' value='" . $arrayOfArrays[$i][0] . "'" . $arrayOfArrays[$i][0] . "'>
+				Delete Profile
+			  </button>"; // goes "updateUser.php"
+			
 		}
 		echo "</div></div>";
 	}
